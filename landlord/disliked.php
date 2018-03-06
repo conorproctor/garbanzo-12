10daa6cfc8143b385eeed47b6591195e21e4fdb4
Conor Proctor <conorproctor@hotmail.com>
2018-03-06 16:16:02 +0000

    first commit

---
+++ landlord/disliked.php
@@ -0,0 +1,18 @@
+<?php
+include 'connection.php';
+session_start();?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>DIS LIKED</title>
+    <h1>Disliked houses</h1>
+    <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body style="background-image:url('img/backimg.jpg')">
+  </head>
+</head>
+
+
+</html>
---
+++ landlord/liked.php
@@ -0,0 +1,18 @@
+<?php
+include 'connection.php';
+session_start();?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>LIKED</title>
+    <h1>Liked houses</h1>
+    <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body style="background-image:url('img/backimg.jpg')">
+  </head>
+</head>
+
+
+</html>
